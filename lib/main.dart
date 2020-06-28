a0fda3cb6655c9ed990da74f20a603a3d12574ca
AliAlEid <ali.t.aleid@gmail.com>
2020-06-28 09:39:29 +0300

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
-import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/chat_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -10,6 +11,13 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: 'WelcomeScreen',
+      routes: {
+        'WelcomeScreen': (context) => WelcomeScreen(),
+        'LoginScreen': (context) => LoginScreen(),
+        'RegistrationScreen': (context) => RegistrationScreen(),
+        'ChatScreen': (context) => ChatScreen(),
+      },
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
